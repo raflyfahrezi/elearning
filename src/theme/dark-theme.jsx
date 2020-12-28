6510fc15dddc2ee9d27c12f23cc22b700bca4ae7
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-28 14:10:21 +0700

    adding dark and light theme

---
+++ src/theme/dark-theme.jsx
@@ -0,0 +1,11 @@
+const DarkTheme = {
+    color: {
+        navigation: '#1A1A1A',
+        body: '#1F1F1F',
+        card: '#242731',
+        text: '#FFFFFF',
+        icon: '#FFFFFF',
+    },
+}
+
+export default DarkTheme
---
+++ src/theme/light-theme.jsx
@@ -0,0 +1,11 @@
+const LightTheme = {
+    color: {
+        navigation: '#FDFDFD',
+        body: '#FDFDFD',
+        card: '#FFFFFF',
+        text: '#121212',
+        icon: '#121212',
+    },
+}
+
+export default LightTheme
