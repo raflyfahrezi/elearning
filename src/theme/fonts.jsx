bcd745ba5dddfe12f3c6772850c24c022c926e66
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-28 14:24:43 +0700

    adding fonts.jsx

---
+++ src/theme/fonts.jsx
@@ -0,0 +1,23 @@
+const Fonts = {
+    fontFamily: '"Poppins"',
+    fontSize: {
+        12: '12px',
+        14: '14px',
+        16: '16px',
+        20: '20px',
+        24: '24px',
+        32: '32px',
+        48: '48px',
+    },
+    lineSpacing: {
+        12: '19.416px',
+        14: '22.652px',
+        16: '25.888px',
+        20: '32.360px',
+        24: '38.832px',
+        32: '51.776px',
+        48: '77.664px',
+    },
+}
+
+export default Fonts
