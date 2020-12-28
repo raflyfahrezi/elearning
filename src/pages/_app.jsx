9245173cb0a3d8adb7bf485eba38007a8447c29a
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-28 14:14:20 +0700

    implement theme and font in _app.js

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import '@fontsource/poppins'
 
 import GlobalStyles from '../styles/globalStyles'
 
+import LightTheme from '../theme/light-theme'
+import DarkTheme from '../theme/dark-theme'
+
 const MyApp = ({ Component, pageProps }) => {
+    const [theme, setTheme] = useState('default')
+
     return (
         <Fragment>
-            <GlobalStyles />
-            <Component {...pageProps} />
+            <ThemeProvider theme={theme === 'default' ? LightTheme : DarkTheme}>
+                <GlobalStyles />
+                <Component {...pageProps} />
+            </ThemeProvider>
         </Fragment>
     )
 }
