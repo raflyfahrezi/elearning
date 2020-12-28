f34a5954cf798a96dda8c343bbe593b6384436be
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-28 20:44:00 +0700

    create menu.jsx

---
+++ src/content/menu.jsx
@@ -0,0 +1,16 @@
+const Menu = [
+    {
+        name: 'Home',
+    },
+    {
+        name: 'Calendar',
+    },
+    {
+        name: 'My Courses',
+    },
+    {
+        name: 'Course List',
+    },
+]
+
+export default Menu
