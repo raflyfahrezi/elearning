264c22e44ab6af946ca0957a919da920625f42be
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-29 13:39:18 +0700

    edit text component

--- src/components/typography/text.jsx
+++ src/components/typography/text.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Text = styled['div']`
+const Text = styled['p']`
     font-family: ${props => props['theme']['fontFamily']};
     font-size: ${props => props['theme']['fontSize']['16']};
 
