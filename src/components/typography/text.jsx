9b819bf28c30445325922d98f8fafeb8e627fbc6
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-29 13:30:23 +0700

    create text component

---
+++ src/components/typography/text.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Text = styled['div']`
+    font-family: ${props => props['theme']['fontFamily']};
+    font-size: ${props => props['theme']['fontSize']['16']};
+
+    color: ${props => props['theme']['color']['text']};
+`
+
+const text = ({ children }) => {
+    return <Text>{children}</Text>
+}
+
+export default text
