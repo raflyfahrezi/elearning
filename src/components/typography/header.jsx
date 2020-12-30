dd826842432d8e5aebb964724b24f388531c735d
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-30 07:53:14 +0700

    creating header component

---
+++ src/components/typography/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled['p']`
+    font-family: ${props => props['theme']['fontFamily']};
+    font-size: ${props => props['theme']['fontSize']['16']};
+    font-weight: bold;
+
+    color: ${props => props['theme']['color']['text']};
+`
+
+const header = ({ children }) => {
+    return <Header>{children}</Header>
+}
+
+export default header
