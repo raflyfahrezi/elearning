a912e3a2a6fcab04145ab101b52b15b0125f6d96
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-30 08:17:42 +0700

    edit course overview

--- src/components/module/home/mainContent/components/courseOverview.jsx
+++ src/components/module/home/mainContent/components/courseOverview.jsx
@@ -2,15 +2,45 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Header from '../../../../typography/header'
+import Text from '../../../../typography/text'
+
+import MataKuliahLists from '../../../../../content/mataKuliah.json'
 
 const CourseOverview = styled['div']`
     width: 100%;
+`
+
+const CourseOverviewContent = styled['div']`
+    width: 100%;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(190px, 1fr));
+`
+
+const CourseOverviewCard = styled['div']`
+    width: 190px;
+    height: 190px;
+
+    padding: 20px;
+
+    background-color: ${props => props['theme']['color']['card']};
 `
 
 const courseOverview = () => {
     return (
         <CourseOverview>
             <Header>Course Overview</Header>
+            <CourseOverviewContent>
+                {MataKuliahLists.map((item, index) => {
+                    return (
+                        <CourseOverviewCard key={index}>
+                            <Text>
+                                <b>{item.nama}</b>
+                            </Text>
+                        </CourseOverviewCard>
+                    )
+                })}
+            </CourseOverviewContent>
         </CourseOverview>
     )
 }
